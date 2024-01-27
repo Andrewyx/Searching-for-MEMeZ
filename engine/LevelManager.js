556950162d7941484c9b8f9adb629b8a50038396
Andrew Mao <andrewyxmao@gmail.com>
2024-01-27 00:24:35 -0800

    test

--- engine/LevelManager.js
+++ engine/LevelManager.js
@@ -24,30 +24,5 @@ export class LevelManager {
         this.level = level;
         let newWin = open(html, '_blank', `width=${width},height=${height},menubar=no,toolbar=no,status=no,scrollbars=yes,screenX= 
                           + ${x} + ,screenY= + ${y}`)
-        newWin.onload = function() {
-            let preloadHTML =
-            `<script type="module">
-                import {TestLevel} from './levels/TestLevel.js';
-                import {StartLevel} from './levels/StartLevel.js';
-                import {resizeCanvas} from './engine/utils/CanvasUtils.js';
-                import { FirstLevel } from './levels/FirstLevel.js';
-                import { LevelManager } from './engine/LevelManager.js';
-                const canvas = document.getElementById("mainCanvas")
-
-                const ctx = canvas.getContext('2d');
-
-                // resize canvas to get better image quality
-                resizeCanvas(canvas, ctx);
-
-                window.addEventListener('resize', () => resizeCanvas(canvas, ctx));
-
-                // TestLevel.preload(() => console.log("All assets are loaded")).then(() => TestLevel.startLevel(ctx))
-                FirstLevel.preload(() => console.log("All assets are loaded")).then(() => FirstLevel.startLevel(ctx))
-            </script>`;
-            newWin.document.body.insertAdjacentHTML('afterend', preloadHTML);
-                   
-            };
     }
-
-
 }
